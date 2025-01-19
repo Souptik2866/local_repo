c4c38b6427f08f48fbca3aec4225a15f637f4c35
Souptik2866 <souptikmitra2866@@>
2025-01-19 14:55:43 +0530

    functions with parameters and return type

--- start_functions.c
+++ start_functions.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
-int add(int a, int b)
+#include <stdio.h>              // standard input/output header library.
+int sum(long int a, long int b) // defining a function named as "sum" with parameters 'a' and 'b'.
 {
-    return a + b;
+    return a + b; // returning the function to where it was called.
 }
-int main()
+int main() // main function.
 {
-    int c;
-    c = add(4, 5);
-    printf("the addition is %d", c);
+    long int num_1, num_2, c;
+    printf("Enter number 1 : ");
+    scanf("%d", &num_1);
+    printf("Enter number 2 : ");
+    scanf("%d", &num_2);
+    c = sum(num_1, num_2);             // calling statement with parameters num_1,num_2.
+    printf("the addition is : %d", c); // printing the value of c.
     return 0;
 }
